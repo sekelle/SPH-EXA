a393ccf7393afacac2d7033103facdfc3163c366
nknk567 <noah.kubli@uzh.ch>
2024-06-11 21:16:33 +0200

    debug

--- sph/include/sph/particles_data_gpu.cuh
+++ sph/include/sph/particles_data_gpu.cuh
@@ -101,7 +101,8 @@ public:
     DevVector<KeyType>   keys;                               // Particle space-filling-curve keys
     DevVector<unsigned>  nc;                                 // number of neighbors of each particle
     DevVector<HydroType> dV11, dV12, dV13, dV22, dV23, dV33; // Velocity gradient components
-    DevVector<unsigned>   particleIDs;
+    DevVector<KeyType>   particleIDs;
+    DevVector<KeyType>   keyTypeSwap;
 
     //! @brief SPH interpolation kernel lookup tables
     DevVector<HydroType> wh, whd;
@@ -119,7 +120,7 @@ public:
         "x",     "y",        "z",    "x_m1", "y_m1", "z_m1", "vx",   "vy",   "vz",   "rho",   "u",    "p",
         "prho",  "tdpdTrho", "h",    "m",    "c",    "ax",   "ay",   "az",   "du",   "du_m1", "c11",  "c12",
         "c13",   "c22",      "c23",  "c33",  "mue",  "mui",  "temp", "cv",   "xm",   "kx",    "divv", "curlv",
-        "alpha", "gradh",    "keys", "nc",   "dV11", "dV12", "dV13", "dV22", "dV23", "dV33", "particleIDs"};
+        "alpha", "gradh",    "keys", "nc",   "dV11", "dV12", "dV13", "dV22", "dV23", "dV33", "particleIDs", "keyTypeSwap"};
 
     /*! @brief return a tuple of field references
      *
@@ -129,7 +130,7 @@ public:
     {
         auto ret = std::tie(x, y, z, x_m1, y_m1, z_m1, vx, vy, vz, rho, u, p, prho, tdpdTrho, h, m, c, ax, ay, az, du,
                             du_m1, c11, c12, c13, c22, c23, c33, mue, mui, temp, cv, xm, kx, divv, curlv, alpha, gradh,
-                            keys, nc, dV11, dV12, dV13, dV22, dV23, dV33, particleIDs);
+                            keys, nc, dV11, dV12, dV13, dV22, dV23, dV33, particleIDs, keyTypeSwap);
 
         static_assert(std::tuple_size_v<decltype(ret)> == fieldNames.size());
         return ret;
